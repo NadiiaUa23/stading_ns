f916924f0c8db5343db44d9555c35aa41393736b
NadiiaUa23 <hasiminka92@gmail.com>
2026-05-26 19:34:08 +0200

    Заняття 13. Вкладені маршрути

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,8 +3,23 @@ export const metadata = {
   description: 'This is the profile page',
 };
 
+import Link from 'next/link';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <section>
+      <h1>My Profile</h1>
+      <h2>Name: User name</h2>
+      <p>
+        Some description: Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Cumque non quis, vero consectetur eum at commodi facere error,
+        laborum, rerum labore corrupti neque veritatis sed minima et nam. Autem,
+        cumque.
+      </p>
+
+      <Link href="/profile/edit">Edit profile</Link>
+    </section>
+  );
 };
 
 export default Profile;
