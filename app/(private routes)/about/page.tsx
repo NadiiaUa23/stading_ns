49fc8688cc33416187cf5de14386f28e649ceaa3
NadiiaUa23 <hasiminka92@gmail.com>
2026-07-20 17:32:21 +0200

    add redirect

--- app/(private routes)/about/page.tsx
+++ app/(private routes)/about/page.tsx
@@ -1,10 +1,26 @@
-export const metadata = {
-  title: 'About',
-  description: 'This is the about page',
+// export const metadata = {
+//   title: 'About',
+//   description: 'This is the about page',
+// };
+
+// const About = () => {
+//   return <div>About</div>;
+// };
+
+// export default About;
+
+// app/about/page.tsx
+
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'About page',
 };
 
 const About = () => {
-  return <div>About</div>;
+  redirect('/');
+  return null;
 };
 
 export default About;
